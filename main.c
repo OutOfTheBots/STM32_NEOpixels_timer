713d98b4bd32558cd2252020c9f78fa7b9704c13
OutOfTheBots <30621667+OutOfTheBots@users.noreply.github.com>
2020-04-26 08:44:52 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /**
-Copyright (c) 2018 Out of the BOTS
+Copyright (c) 2020 Out of the BOTS
 MIT License (MIT) 
 Author: Shane Gingell
 Permission is hereby granted, free of charge, to any person obtaining a copy
